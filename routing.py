cba0f22a2922fe2fe8af5fbabe308c04e43ef117
ruchinew253-ctrl <ruchinew253@gmail.com>
2026-09-18 18:59:19 +0530

    Update routing.py

--- routing.py
+++ routing.py
@@ -3,7 +3,7 @@
 
 ROUTING_TABLE = {
 
-    "918595921795": {
+    "918851428184": {
         "client_name": "Ruchee",
         "assigned_to": "Saksham Malhotra",
         "teams_webhook_url": "https://defaultf9cac348195d4c77ac88b33b9611e3.d5.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/29/workflows/425b03d880c54b9c81c27adebafd934f/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=jQI5SfazQuRvjnoVmx251K17sBt1zWR0XB0mCmAU1I",
